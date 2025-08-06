28463576bd37a84d93cfcf08d55cd2683ea92364
Jônatas Fernandes <128390310+jonatasfernandessilva7@users.noreply.github.com>
2025-08-06 12:17:05 -0300

    fix(main): caminho api

--- source/api.py
+++ source/api.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import uuid
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from fastapi import APIRouter, HTTPException, Request, UploadFile, File
-from controller_audio import receber_e_processar_audio
+from fastapi.responses import JSONResponse
+from fastapi import APIRouter, HTTPException, Request
+from controller_audio import iniciarGravacao, receber_e_processar_audio
 
 router = APIRouter(
     prefix="/v1"
@@ -15,11 +17,22 @@ router = APIRouter(
 async def read_root():
     return {"message": "Service is running!"}
 
-
-@router.post("/upload-audio")
-async def audio_upload_e_processamento(request:Request, file: UploadFile = File(...)):
+@router.post("/iniciar-gravacao")
+async def receber_audio():
     try:
-        return await receber_e_processar_audio(request, file)
+        request_idempotency_key = str(uuid.uuid4())
+        await iniciarGravacao(idempotency_key=request_idempotency_key)
+        return JSONResponse({"status": 200, "message": "Gravação iniciada com sucesso."})
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao iniciar gravação: {e}")
+
+
+@router.post("/parar-gravacao")
+async def parar_gravacao(request:Request):
+    try:
+        return await receber_e_processar_audio(request)
     except HTTPException as e:
         raise e  # Re-raise HTTPExceptions
     except Exception as e:
